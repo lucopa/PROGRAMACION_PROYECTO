d636eb095d242702727d14128ea4c7e9e5673cc6
Luis <lcobpar754@g.educaand.es>
2023-04-28 16:11:50 +0200

    insertar datos cript

---
+++ insert_datos.sql
@@ -0,0 +1,6 @@
+INSERT INTO espacio (id_espacio,descripcion,capacidad,ordenadores,pizarra,proyector,nombre,codigo_reserva) VALUES (1,"clase1",15,0,1,1,1.1,NULL);
+INSERT INTO espacio (id_espacio,descripcion,capacidad,ordenadores,pizarra,proyector,nombre,codigo_reserva) VALUES (2,"clase2",10,1,1,1,1.2,NULL);
+INSERT INTO espacio (id_espacio,descripcion,capacidad,ordenadores,pizarra,proyector,nombre,codigo_reserva) VALUES (3,"clase3",20,0,0,1,2.2,NULL);
+INSERT INTO espacio (id_espacio,descripcion,capacidad,ordenadores,pizarra,proyector,nombre,codigo_reserva) VALUES (4,"clase4",25,0,1,0,2.1,NULL);
+INSERT INTO espacio (id_espacio,descripcion,capacidad,ordenadores,pizarra,proyector,nombre,codigo_reserva) VALUES (5,"clase5",17,1,1,1,0.1,NULL);
+
